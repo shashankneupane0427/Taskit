b8566f18fb42e25ae443e5707491e7c476887c61
Shashank Neupane <suryaneupane0427@gmail.com>
2024-12-06 23:25:22 +0545

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
-/** @type {import('tailwindcss').Config} */
+
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+
   theme: {
+
     extend: {},
+
   },
+
   plugins: [],
+
 }
